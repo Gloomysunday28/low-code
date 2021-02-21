d4d838327241c90f2bc21f350b9a07f5f2d4d836
cjd <459953103@qq.com>
2021-02-21 18:20:17 +0800

    :zap: feature: 1. 增加组件编辑状态 2. 增加canvas画布 3. 增加canvas选定区域内的组件变为编辑状态

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,7 +6,13 @@ declare interface LowComponentsProps {
   type?: string,
   compKey?: string,
   name?: string | number,
+  left?: string,
+  top?: string,
   locked?: boolean,
+  edit?: boolean, // 是否可以编辑
   styles?: React.CSSProperties,
+  width?: number | string,
+  height?: number | string,
   onDragEnd?: (e, ...rest?) => void
+  onDragStart?: (e, ...rest?) => void
 }
